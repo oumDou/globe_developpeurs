475c9d7d615e5c9fbc291cbaaf35f3294b9ba0f7
Oumou Doumbia <doumbia.oumou@gmail.com>
2023-08-09 13:54:16 -0400

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'globe_developpeurs',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
